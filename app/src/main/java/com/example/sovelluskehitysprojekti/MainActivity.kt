569fe73a06115add7506c7774f1e1f2d9b35850d
Olli Kokko <olli.t.kokko@gmail.com>
2021-03-02 16:59:56 +0200

    Tein pohjatyötä projektille. Tästä pääsee alkuun (toivottavasti). Lisäsin bottom nav bar systeemin. Se näyttää rikkinäiseltä studiossa mut toimii sit puhelimessa / emulaattorissa.

--- app/src/main/java/com/example/sovelluskehitysprojekti/MainActivity.kt
+++ app/src/main/java/com/example/sovelluskehitysprojekti/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.sovelluskehitysprojekti
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,9 +17,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        val hae = findViewById<Button>(R.id.hae)
+
+        hae.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            startActivity(intent)
         }
     }
 
