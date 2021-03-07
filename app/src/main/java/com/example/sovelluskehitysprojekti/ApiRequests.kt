11e0c6ebe384249ca829b7b82f86b8fe607d2168
Olli Kokko <olli.t.kokko@gmail.com>
2021-03-07 15:52:14 +0200

    Api pohja juttu aloitus jee!

--- app/src/main/java/com/example/sovelluskehitysprojekti/ApiRequests.kt
+++ app/src/main/java/com/example/sovelluskehitysprojekti/ApiRequests.kt
@@ -1,11 +1,12 @@
 package com.example.sovelluskehitysprojekti
 
 import com.example.sovelluskehitysprojekti.api.AuthorJson
+import com.example.sovelluskehitysprojekti.api.NonPresenterAuthor
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiRequests {
 
-    @GET('/v1/search?lookfor=sibelius&type=Author')
-    fun getAuthorRecords(): Call<AuthorJson>
+    @GET("/v1/search?lookfor=sibelius&type=Author")
+    fun getAuthorRecords(): Call<NonPresenterAuthor>
 }
--- app/src/main/java/com/example/sovelluskehitysprojekti/MainActivity2.kt
+++ app/src/main/java/com/example/sovelluskehitysprojekti/MainActivity2.kt
@@ -1,13 +1,22 @@
 package com.example.sovelluskehitysprojekti
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
+import retrofit2.awaitResponse
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val BASE_URL = "https://api.finna.fi/"
+const val BASE_URL = "https://api.finna.fi"
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -18,13 +27,31 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
         setSupportActionBar(findViewById(R.id.toolbar))
         getCurrentData()
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        progressBar.visibility = View.VISIBLE
     }
 
     private fun getCurrentData() {
         val api = Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addCallAdapterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(Api)
+            .create(ApiRequests::class.java)
+
+            GlobalScope.launch(Dispatchers.IO) {
+                val response = api.getAuthorRecords().awaitResponse()
+                if (response.isSuccessful) {
+                    val data = response.body()!!
+                    //Log.d(TAG, data.name)
+
+                    withContext(Dispatchers.Main){
+                        val textView = findViewById<TextView>(R.id.textView)
+                        textView.text = data.role
+                        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+                        progressBar.visibility = View.INVISIBLE
+                    }
+
+                }
+            }
     }
 }
