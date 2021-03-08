0088612025c9af5fe5e5f97d7d484f07e9b84e22
Sam Whitley <sam.whitley@hotmail.com>
2021-03-08 19:52:09 +0200

    MyAdapter

---
+++ app/src/main/java/com/example/sovelluskehitysprojekti/MyAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.sovelluskehitysprojekti
+
+import android.content.Context
+import android.widget.AbsListView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sovelluskehitysprojekti.api.RecordList
+
+class MyAdapter(val context: Context, val recordList: List<RecordList>): RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    class ViewHolder
+    //https://youtu.be/_bVWsL5CHh4?t=247
+}
