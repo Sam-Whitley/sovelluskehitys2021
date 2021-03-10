3fe5a3bdb020f77000fc737143f2845429004a96
Sam Whitley <sam.whitley@hotmail.com>
2021-03-10 19:10:39 +0200

    CardView update

--- app/src/main/java/com/example/sovelluskehitysprojekti/MyAdapter.kt
+++ app/src/main/java/com/example/sovelluskehitysprojekti/MyAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sovelluskehitysprojekti.api.*
+import org.w3c.dom.Text
 
 class MyAdapter(private val records: RecordList) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
 
@@ -14,13 +15,22 @@ class MyAdapter(private val records: RecordList) : RecyclerView.Adapter<MyAdapte
         return ViewHolder(view)
     }
 
-    override fun getItemCount() = records.resultCount
+    override fun getItemCount() = records.records.size
 
+    //resultCount, buildings, formats, nonPresenterAuthors/name: "Sibelius", Suuret säveltäjät title
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.title.text = records.records[position].title
+        holder.format.text = records.records[position].formats[0].translated
+        holder.author.text = records.records[position].nonPresenterAuthors[0].name
+        holder.building0.text = records.records[position].buildings[0].translated
+        holder.building1.text = records.records[position].buildings[1].translated
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val title: TextView = itemView.findViewById(R.id.recordTitle)
+        val format: TextView = itemView.findViewById(R.id.recordResultItemFormat)
+        val author: TextView = itemView.findViewById(R.id.recordAuthor)
+        val building0: TextView = itemView.findViewById(R.id.recordBuilding0)
+        val building1: TextView = itemView.findViewById(R.id.recordBuilding1)
     }
 }
